7320e1287911e3a8933abf2722b35c0627d05ae3
trandushev <trand@abv.bg>
2017-01-16 13:13:43 +0200

    Square of Stars

---
+++ Square of Stars/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= a; i++)
+            {
+                for (int j = 1; j <= a; j++)
+                {
+                    if (i == 1 || i == a || j == 1 || j == a)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
